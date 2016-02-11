12ae78f484b70d9bde920aeaa956b8d8f781b67b
Dirk Vandendriessche <dirk.vandendriessche@thomascookonline.com>
2016-02-11 16:58:31 +0100

    WEB-21681: Proxy change listener needs to publish to different buses based on incoming header value

--- src/main/scala/io/github/tcdl/msb/Msb.scala
+++ src/main/scala/io/github/tcdl/msb/Msb.scala
@@ -1,13 +1,13 @@
 package io.github.tcdl.msb
 
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-
 import akka.actor.ActorSystem
 import akka.actor.ExtendedActorSystem
 import akka.actor.Extension
 import akka.actor.ExtensionId
 import akka.actor.ExtensionIdProvider
-import io.github.tcdl.msb.api.MsbContextBuilder
+import com.typesafe.config.Config
+import io.github.tcdl.msb.api.{MsbContext, MsbContextBuilder}
 import io.github.tcdl.msb.support.Utils
 
 class MsbImpl(system: ActorSystem) extends Extension {
@@ -15,15 +15,19 @@ class MsbImpl(system: ActorSystem) extends Extension {
   // init jackson for marshalling the json body in the payload
   Utils.getJsonObjectMapper.registerModule(DefaultScalaModule)
 
-  val config = MsbConfig(system).msbConfig
+  private val config = MsbConfig(system)
 
-  val context = new MsbContextBuilder()
-    .withConfig(config)
-    .withShutdownHook(true)
-    .build()
+  val context = buildContext(config.msbConfig)
+  val multiTargetContexts = config.msbTargetsConfig.mapValues(buildContext)
 
-  system.registerOnTermination { context.shutdown() }
-
+  private def buildContext(cfg: Config): MsbContext = {
+    val ctx = new MsbContextBuilder()
+      .withConfig(cfg)
+      .withShutdownHook(true)
+      .build()
+    system.registerOnTermination { ctx.shutdown() }
+    ctx
+  }
 }
 
 object Msb extends ExtensionId[MsbImpl] with ExtensionIdProvider {
--- src/test/scala/io/github/tcdl/msb/MsbRequesterTest.scala
+++ src/test/scala/io/github/tcdl/msb/MsbRequesterTest.scala
@@ -4,7 +4,7 @@ import java.util.UUID
 
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
-import io.github.tcdl.msb.MsbRequester.{Request, Response}
+import io.github.tcdl.msb.MsbRequester.{Request, Response, Responses, TargetNotConfigured}
 import io.github.tcdl.msb.api.message.payload.Payload.Builder
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FlatSpecLike, Matchers}
 
@@ -15,29 +15,32 @@ class MsbRequesterTest extends TestKit(ActorSystem("msb-requester-test")) with I
   with FlatSpecLike with Matchers
   with BeforeAndAfter
   with BeforeAndAfterAll {
-  
-  val msbcontext = Msb(system).context
-  
+
+  val msbContext = Msb(system).context
+  val msbContexts = Msb(system).multiTargetContexts
   var namespace: String = _
-  
+
   before {
     namespace = s"msb-test:requester:${UUID.randomUUID.toString}"
   }
-  
-  override def afterAll(): Unit = msbcontext.shutdown()
-  
+
+  override def afterAll(): Unit = {
+    msbContext.shutdown()
+    msbContexts.foreach(_._2.shutdown())
+  }
+
   "An MsbRequester" should "build a payload and send it into the bus" in {
 
     val requester = system.actorOf(MsbRequester.props(namespace))
-    
+
     // given: a responder to catch the request
-    val request = Promise[String]() 
-    val responder = msbcontext.responderServer(namespace) { (req, responder) => request.success(req.bodyAs[String]) }
+    val request = Promise[String]()
+    val responder = msbContext.responderServer(namespace) { (req, responder) => request.success(req.bodyAs[String]) }
     responder.listen()
 
     // when: the requester sends out a request
     requester ! Request("ping")
-    
+
     // then: it's received by the responder
     Await.result(request.future, 5.seconds) shouldBe "ping"
   }
@@ -45,11 +48,32 @@ class MsbRequesterTest extends TestKit(ActorSystem("msb-requester-test")) with I
   it should "reply if there's a response" in {
     val opts = MsbRequestOptions().withWaitForResponses(1)
     val requester = system.actorOf(MsbRequester.props(namespace, opts))
-    val responder = msbcontext.responderServer(namespace) { (req, responder) => responder.send(new Builder().withBody("pong").build()) }
+    val responder = msbContext.responderServer(namespace) { (req, responder) => responder.send(new Builder().withBody("pong").build()) }
     responder.listen()
-    
+
     requester ! Request("ping")
     expectMsgClass(classOf[Response]).body[String] shouldBe Some("pong")
+    expectMsgClass(classOf[Responses])
   }
-  
+
+  it should "reply from the correct target based on target id" in {
+    val opts = MsbRequestOptions().withWaitForResponses(1)
+    val targetId = "target1"
+    val requester = system.actorOf(MsbRequester.props(namespace = namespace, options = opts))
+    val responder = msbContexts(targetId).responderServer(namespace) { (req, responder) => responder.send(new Builder().withBody("pong").build()) }
+    responder.listen()
+
+    requester ! Request(body = Some("ping"), targetId = Some(targetId))
+    expectMsgClass(classOf[Response]).body[String] shouldBe Some("pong")
+    expectMsgClass(classOf[Responses])
+  }
+
+  it should "reply TargetNotConfigured when there is no configuration for provided target id" in {
+    val opts = MsbRequestOptions().withWaitForResponses(1)
+    val requester = system.actorOf(MsbRequester.props(namespace = namespace, options = opts))
+
+    requester ! Request(body = Some("ping"), targetId = Some("missing_target_id"))
+    expectMsg(TargetNotConfigured("missing_target_id"))
+  }
+
 }
